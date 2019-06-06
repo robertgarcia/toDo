f7fb5df10ca0f442a0ad0b614f38937199939aa4
Roberto D. Garcia <robertdavid_mgt@hotmail.es>
2019-06-06 16:08:49 -0600

    Localstorage - Hacer persistente la información

--- src/app/services/to-do.service.ts
+++ src/app/services/to-do.service.ts
@@ -10,13 +10,25 @@ export class ToDoService {
 
   constructor() { 
     console.log('Servicio Inicializado');
-    const lista1 = new Lista('Terminar Curso de Udemy');
-    const lista2 = new Lista('Terminar el examen de FullStack de Turing');
-    this.listas.push(lista1, lista2);
+    this.cargarStorage();
+    // const lista1 = new Lista('Terminar Curso de Udemy');
+    // const lista2 = new Lista('Terminar el examen de FullStack de Turing');
+    // this.listas.push(lista1, lista2);
   }
 
   crearLista(titulo: string){
     const nuevaLista = new Lista(titulo);
     this.listas.push(nuevaLista);
+    this.guardarStorage();
+  }
+
+  guardarStorage(){
+    localStorage.setItem('data', JSON.stringify(this.listas));
+  }
+
+  cargarStorage(){
+    if(localStorage.getItem('data')){
+      this.listas = JSON.parse(localStorage.getItem('data'));
+    }
   }
 }
