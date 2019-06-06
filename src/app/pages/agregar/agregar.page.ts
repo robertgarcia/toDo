0d3ce82093752636666d0135c048047e07b05031
Roberto D. Garcia <robertdavid_mgt@hotmail.es>
2019-06-06 17:27:47 -0600

    Detalles estéticos de la pantalla de agregar

--- src/app/pages/agregar/agregar.page.ts
+++ src/app/pages/agregar/agregar.page.ts
@@ -33,4 +33,16 @@ export class AgregarPage implements OnInit {
     this.deseosService.guardarStorage();
   }
 
+  cambioCheck(item: ListaItem){
+    const pendientes = this.lista.items.filter(itemData => !itemData.completado).length;
+    if(pendientes === 0){
+      this.lista.terminadaEn = new Date();
+      this.lista.terminada = true;
+    }else{
+      this.lista.terminadaEn = null;
+      this.lista.terminada = false;
+    }
+    this.deseosService.guardarStorage();
+  }
+
 }
