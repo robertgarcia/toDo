19a0a167ff36f7dde9507cce9387e53747262a3d
Roberto D. Garcia <robertdavid_mgt@hotmail.es>
2019-06-06 20:13:12 -0600

    Editar el titulo de las listas
    Version Beta terminada

--- src/app/components/listas/listas.component.ts
+++ src/app/components/listas/listas.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { ToDoService } from '../../services/to-do.service';
 import { Lista } from '../../models/lista.model';
 import { Router } from '@angular/router';
+import { AlertController, IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-listas',
@@ -10,9 +11,11 @@ import { Router } from '@angular/router';
 })
 export class ListasComponent implements OnInit {
   @Input() terminada = true;
+  @ViewChild( IonList ) lista: IonList;
   constructor(
     public toDoService: ToDoService,
     private router:Router,
+    private alertCtrl: AlertController
   ) { }
 
   ngOnInit() {}
@@ -27,6 +30,38 @@ export class ListasComponent implements OnInit {
 
   borrarLista(item:Lista){
     this.toDoService.borrarLista(item);
+  }
+
+  async editarLista(item:Lista){
+    const alert = await this.alertCtrl.create({
+      header: 'Editar lista',
+      inputs:[{
+        name: 'titulo',
+        type: 'text',
+        placeholder: 'Nombre de la lista',
+        value: item.titulo
+      }],
+      buttons: [{
+        text: 'Cancelar',
+        role: 'cancel',
+        handler: ()=>{
+          console.log('Canclear');
+          this.lista.closeSlidingItems();
+        }
+      },{
+        text: 'Editar',
+        handler: (data) =>{
+          if(data.titulo.length === 0){
+            return;
+          }
+          item.titulo = data.titulo;
+          this.toDoService.guardarStorage();
+          this.lista.closeSlidingItems();
+        }
+      }]
+    });
+
+    await alert.present();
   }
 
 }
